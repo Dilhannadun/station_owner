f1ea6c861d1708375b904ea80a75010ec9f8156f
P.D.G.N.T.D. Dharmasinghe <dilhannadun777@gmail.com>
2022-10-14 22:33:54 +0530

    Register and login UI done

--- app/src/main/java/com/example/station_owner/MainActivity.java
+++ app/src/main/java/com/example/station_owner/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.station_owner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button loginRegisterBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginRegisterBtn = (Button) findViewById(R.id.login_register_btn);
+        loginRegisterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openRegisterScreen();
+            }
+        });
+    }
+
+    public void openRegisterScreen(){
+        Intent intent = new Intent(this,RegisterActivity.class);
+        startActivity(intent);
     }
 }
